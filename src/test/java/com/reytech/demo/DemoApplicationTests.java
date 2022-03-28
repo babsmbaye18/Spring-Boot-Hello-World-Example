745e82feab9115614933a523ca76b7667cea2446
babsmbaye18 <40393271+babsmbaye18@users.noreply.github.com>
2022-03-28 23:29:52 +0200

    Update DemoApplicationTests.java

--- src/test/java/com/reytech/demo/DemoApplicationTests.java
+++ src/test/java/com/reytech/demo/DemoApplicationTests.java
@@ -9,5 +9,14 @@ class DemoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	@Test
+        void groupedAssertions() {
+        Personne pers = new Personne("Nassur", "Moumadi");
+
+        assertAll("personne", () -> assertEquals("Nassur", pers.getFirstName()),
+                              () -> assertEquals("Moumadi", pers.getLastName()));
+    }
+
 
 }
